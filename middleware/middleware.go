c336e914f64c8f310b74a6d56d9e374c06bcca95
Nicolas Palpacuer <nicolas.palpacuer@opendoor.com>
2020-07-19 21:46:35 -0400

    Add middleware package for easy custom DBConn
    midleware package to allow inject middleware to pggen without having to recreate a full custom implementation of DBConn.
    For now only Exec has a middleware

---
+++ middleware/middleware.go
@@ -0,0 +1,101 @@
+package middleware
+
+import (
+	"context"
+	"database/sql"
+	"database/sql/driver"
+	"time"
+
+	"github.com/opendoor-labs/pggen"
+)
+
+type ExecFunc func(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
+type ExecMiddleware func(ExecFunc) ExecFunc
+
+// ChainExecMiddleware creates a single exec middleware from a chain
+// of multiple middlewares. Execution is done is done left to right
+func ChainExecMiddleware(execMiddlewares ...ExecMiddleware) ExecMiddleware {
+	return func(next ExecFunc) ExecFunc {
+		for i := len(execMiddlewares) - 1; i >= 0; i-- {
+			next = execMiddlewares[i](next)
+		}
+		return next
+	}
+}
+
+type DBConnWrapper struct {
+	dbConn         pggen.DBConn
+	execMiddleware ExecMiddleware
+}
+
+func WrapDBConn(dbConn pggen.DBConn) *DBConnWrapper {
+	return &DBConnWrapper{
+		dbConn: dbConn,
+	}
+}
+
+func (dbConnWrapper *DBConnWrapper) WithExecMiddleware(execMiddleware ExecMiddleware) *DBConnWrapper {
+	dbConnWrapper.execMiddleware = execMiddleware
+	return dbConnWrapper
+}
+
+// ExecContext apply the midlle ware if any and execute ExecContext on the wrapped DBConn
+func (dbConnWrapper *DBConnWrapper) ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
+	execFunc := func(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
+		return dbConnWrapper.dbConn.ExecContext(ctx, query, args)
+	}
+	if dbConnWrapper.execMiddleware != nil {
+		execFunc = dbConnWrapper.execMiddleware(execFunc)
+	}
+	return execFunc(ctx, query, args)
+}
+
+// Unchanged
+
+func (dbConnWrapper *DBConnWrapper) PrepareContext(ctx context.Context, query string) (*sql.Stmt, error) {
+	return dbConnWrapper.dbConn.PrepareContext(ctx, query)
+}
+
+func (dbConnWrapper *DBConnWrapper) QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error) {
+	return dbConnWrapper.dbConn.QueryContext(ctx, query, args)
+}
+
+func (dbConnWrapper *DBConnWrapper) QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row {
+	return dbConnWrapper.dbConn.QueryRowContext(ctx, query, args)
+}
+
+func (dbConnWrapper *DBConnWrapper) BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error) {
+	return dbConnWrapper.dbConn.BeginTx(ctx, opts)
+}
+
+func (dbConnWrapper *DBConnWrapper) Close() error {
+	return dbConnWrapper.dbConn.Close()
+}
+
+func (dbConnWrapper *DBConnWrapper) Conn(ctx context.Context) (*sql.Conn, error) {
+	return dbConnWrapper.dbConn.Conn(ctx)
+}
+
+func (dbConnWrapper *DBConnWrapper) Driver() driver.Driver {
+	return dbConnWrapper.dbConn.Driver()
+}
+
+func (dbConnWrapper *DBConnWrapper) PingContext(ctx context.Context) error {
+	return dbConnWrapper.dbConn.PingContext(ctx)
+}
+
+func (dbConnWrapper *DBConnWrapper) SetConnMaxLifetime(d time.Duration) {
+	dbConnWrapper.dbConn.SetConnMaxLifetime(d)
+}
+
+func (dbConnWrapper *DBConnWrapper) SetMaxIdleConns(n int) {
+	dbConnWrapper.dbConn.SetMaxIdleConns(n)
+}
+
+func (dbConnWrapper *DBConnWrapper) SetMaxOpenConns(n int) {
+	dbConnWrapper.dbConn.SetMaxOpenConns(n)
+}
+
+func (dbConnWrapper *DBConnWrapper) Stats() sql.DBStats {
+	return dbConnWrapper.dbConn.Stats()
+}
