2c9fec0700efb73d92b4c968e6194d1063f5449e
Nicolas Palpacuer <nicolas.palpacuer@opendoor.com>
2020-09-14 11:46:27 -0400

    update mock generation

--- tools/test.bash
+++ tools/test.bash
@@ -28,6 +28,7 @@ createdb -h "${DB_HOST}" -W test -U postgres -w -e pggen_test 2>/dev/null || /bi
 go generate ./...
 
 psql "$DB_URL" < cmd/pggen/test/db.sql
+go generate ./testing/mocks/...
 
 if [[ -n "${LINT+x}" ]] ; then
     golangci-lint run -E gofmt -E gosec -E gocyclo -E deadcode
