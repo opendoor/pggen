2a3916b77eb70039f1331742ecf2743e75af7959
Nicolas Palpacuer <nicolas.palpacuer@opendoor.com>
2020-09-14 11:18:46 -0400

    Use go base testing package

--- middleware/middleware_test.go
+++ middleware/middleware_test.go
@@ -3,55 +3,61 @@ package middleware_test
 import (
 	"context"
 	"database/sql"
-
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"reflect"
+	"testing"
 
 	"github.com/opendoor-labs/pggen/middleware"
 	"github.com/opendoor-labs/pggen/testing/mocks"
 )
 
-var _ = Describe("Middleware", func() {
-	var (
-		mockDBConn *mocks.DBConn
-	)
+func TestExecMiddleware(t *testing.T) {
+	mockDBConn := &mocks.DBConn{}
+	expectedCtx := context.Background()
+	expectedQuery := "myQuery"
+	expectedArgs := []interface{}{1, "a"}
+	expectedResult := &mocks.Result{}
 
-	BeforeEach(func() {
-		mockDBConn = &mocks.DBConn{}
-	})
-
-	Describe("ExecMiddleware", func() {
-		expectedCtx := context.Background()
-		expectedQuery := "myQuery"
-		expectedArgs := []interface{}{1, "a"}
-		expectedResult := &mocks.Result{}
-
-		It("applies the middleware provided to the wrapper", func() {
-			beforeExecDone := false
-			afterExecDone := false
-			testExecMiddleware := func(execFunc middleware.ExecFunc) middleware.ExecFunc {
-				return func(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
-					Expect(ctx).To(Equal(expectedCtx))
-					Expect(query).To(Equal(expectedQuery))
-					Expect(args).To(Equal(expectedArgs))
-					beforeExecDone = true
-					result, err := execFunc(ctx, query, args)
-					Expect(result).To(Equal(expectedResult))
-					Expect(err).NotTo(HaveOccurred())
-					afterExecDone = true
-					return result, err
-				}
+	beforeExecDone := false
+	afterExecDone := false
+	testExecMiddleware := func(execFunc middleware.ExecFunc) middleware.ExecFunc {
+		return func(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
+			if !reflect.DeepEqual(ctx, expectedCtx) {
+				t.Fatalf("context do not match, expected: %v, got: %v", expectedCtx, ctx)
 			}
-			connWrapper := middleware.NewDBConnWrapper(mockDBConn).WithExecMiddleware(testExecMiddleware)
+			if query != expectedQuery {
+				t.Fatalf("query does not match, expected: %s, got: %s", expectedQuery, query)
+			}
+			if !reflect.DeepEqual(args, expectedArgs) {
+				t.Fatalf("args does not match, expected: %v, got: %v", expectedArgs, args)
+			}
+			beforeExecDone = true
+			result, err := execFunc(ctx, query, args)
+			if err != nil {
+				t.Fatalf("exec function returned an error: %v", err)
+			}
+			if result != expectedResult {
+				t.Fatalf("result does not match, expected: %v, got: %v", expectedResult, result)
+			}
+			afterExecDone = true
+			return result, err
+		}
+	}
+	connWrapper := middleware.NewDBConnWrapper(mockDBConn).WithExecMiddleware(testExecMiddleware)
 
-			mockDBConn.ExecContextReturns(expectedResult, nil)
+	mockDBConn.ExecContextReturns(expectedResult, nil)
 
-			result, err := connWrapper.ExecContext(expectedCtx, expectedQuery, expectedArgs...)
-			Expect(result).To(Equal(expectedResult))
-			Expect(err).NotTo(HaveOccurred())
+	result, err := connWrapper.ExecContext(expectedCtx, expectedQuery, expectedArgs...)
+	if err != nil {
+		t.Fatalf("exec function returned an error: %v", err)
+	}
+	if result != expectedResult {
+		t.Fatalf("result does not match, expected: %v, got: %v", expectedResult, result)
+	}
 
-			Expect(beforeExecDone).To(BeTrue())
-			Expect(afterExecDone).To(BeTrue())
-		})
-	})
-})
+	if !beforeExecDone {
+		t.Fatalf("before execution did not happen")
+	}
+	if !afterExecDone {
+		t.Fatalf("after execution did not happen")
+	}
+}
