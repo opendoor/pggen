e8d8cd2c6911342c8f968482b170490eeec1381b
Nicolas Palpacuer <nicolas.palpacuer@opendoor.com>
2020-09-15 10:11:35 -0400

    check in mocks

--- middleware/internal/mocks/generate.go
+++
@@ -1,4 +0,0 @@
-package mocks
-
-//go:generate counterfeiter -o=dbconn.mock.go -fake-name=DBConn github.com/opendoor-labs/pggen.DBConn
-//go:generate counterfeiter -o=result.mock.go -fake-name=Result database/sql.Result
