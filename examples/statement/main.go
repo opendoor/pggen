57f45796d62c38a5ecb733cd050949d864af9566
Ethan Pailes <ethan@pailes.org>
2020-10-08 08:22:08 -0400

    add example for statements
    Closes #105

---
+++ examples/statement/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+	"github.com/opendoor-labs/pggen"
+	"github.com/opendoor-labs/pggen/examples/query_argument_names/models"
+)
+
+func main() {
+	ctx := context.Background()
+
+	conn, err := sql.Open("postgres", os.Getenv("DB_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pgClient := models.NewPGClient(conn)
+
+	id, err := pgClient.InsertUser(ctx, &models.User{
+		Email:    "alphonso@yehaw.website",
+		Nickname: "Alph",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = pgClient.DeleteUsersByNickname(ctx, "Alph")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = pgClient.GetUser(ctx, id)
+	if err == nil {
+		log.Fatal("Alph is unexpectedly still in the db")
+	}
+	if pggen.IsNotFoundError(err) {
+		fmt.Printf("Alph not found\n")
+	}
+}
