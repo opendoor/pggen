442fbdecd2c44fed99381925438a0a269d4c68de
ethanpailes <ethan@pailes.org>
2021-03-24 15:19:35 -0400

    add MIT licence (#167)
    Legal has given the sign off to release this under the MIT
    licence!

--- unstable/doc.go
+++ unstable/doc.go
@@ -1,3 +1,5 @@
+// (c) 2021 Opendoor Labs Inc.
+// This code is licenced under the MIT licence (see the LICENCE file in the repo root).
 // package unstable contains items that really should be private, but need to
 // be accessable to generated code. Do not use this code directly.
 //
--- unstable/errors.go
+++ unstable/errors.go
@@ -1,3 +1,5 @@
+// (c) 2021 Opendoor Labs Inc.
+// This code is licenced under the MIT licence (see the LICENCE file in the repo root).
 package unstable
 
 // DO NOT USE. Use pggen.IsNotFoundError instead of directly referencing this type.
