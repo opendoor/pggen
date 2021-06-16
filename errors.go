be4ae12f2dc811da0b6639eea2aabeab47fbff98
ethanpailes <ethan@pailes.org>
2021-06-16 09:02:34 -0400

    IsNotFoundError: check causal chain as well (#172)
    This is a breaking change, but it shouldn't impact any
    current existing usage. This will be nice because it means
    you can intercept not-found errors in middleware and wrap them
    with your own error tagging system and calls to IsNotFoundError
    in your existing codebase should continue to work.
    
    This also makes us match the behavior of errors.Is from the standard
    library better, which ought to be less confusing given the name
    of the function.

--- errors.go
+++ errors.go
@@ -9,7 +9,27 @@ import (
 // file: errors.go
 // This file defines error utility functions used by pggen.
 
+// IsNotFoundError returns true if the given error, or any of its causes
+// is a not found error returned from pggen. The causal chain is determined
+// by repeatedly calling Unwrap on the error.
 func IsNotFoundError(err error) bool {
-	_, is := err.(*unstable.NotFoundError)
-	return is
+	for {
+		if err == nil {
+			return false
+		}
+
+		_, is := err.(*unstable.NotFoundError)
+		if is {
+			return is
+		}
+
+		// we don't use errors.Unwrap in order to maintain our msgv
+		u, ok := err.(interface {
+			Unwrap() error
+		})
+		if !ok {
+			return false
+		}
+		err = u.Unwrap()
+	}
 }
